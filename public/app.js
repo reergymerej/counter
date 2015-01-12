99e6061956fb2ff60511611c2a009987f601ad2c
Jeremy Greer <jeremy.greer.atl@gmail.com>
2015-01-11 20:28:29 -0500

    add mongoose, get/send data

--- public/app.js
+++ public/app.js
@@ -1,18 +1,34 @@
 (function () {
   'use strict';
 
-  var Item = function () {
-    this.date = new Date();
-  };
+  angular.module('counter', []).
 
-  angular.module('counter', [])
-
-  .controller('MainCtrl', ['$scope', function ($scope) {
+  controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.best = 8;
     $scope.items = [];
 
+    // Get the "best" from the server.  Use $http instead of $resource
+    // because we're won't be doing any CRUD stuff.
+    $http.get('/best').
+      success(function (resp, status, headers, config) {
+        $scope.best = resp;
+      }).
+      error(function (data, status, headers, config) {
+        console.error('unable to get best');
+      });
+
     $scope.add = function () {
-      $scope.items.unshift(new Item());
+      $scope.adding = true;
+      
+      $http.post('/item').
+        success(function (resp, status, headers, config) {
+          $scope.items.unshift(resp);
+          $scope.adding = false;
+        }).
+        error(function (resp, status, headers, config) {
+          console.error('unable to add item');
+          $scope.adding = false;
+        });
     };
   }]);
 }());
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
+
+// persistence stuff (should be moved)
+var mongoose = require('mongoose');
+
+var itemSchema = mongoose.Schema({
+  date: { type: Date, default: Date.now }
+});
+
+var Item = mongoose.model('Item', itemSchema);
+
+// mongoose.connect('mongodb://localhost/counter');
+mongoose.connect('dude:dude@troup.mongohq.com:10027/reergymerej');
+
+
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index.html');
+});
+
+router.get('/best', function (req, res) {
+  res.jsonp({
+    value: 8,
+    date: new Date()
+  });
+});
+
+router.post('/item', function (req, res) {
+  var item = new Item();
+
+  item.save(function (err, item) {
+    if (err) {
+      console.log('ERROR!', err);
+    } else {
+      res.jsonp(item);
+    }
+  });
 });
 
 module.exports = router;
